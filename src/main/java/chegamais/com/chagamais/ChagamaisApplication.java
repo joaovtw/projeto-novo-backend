91c366490b8aa4d106f7e76c36bbd920a836dc22
Luan Carvalho Pedrosa <luan.pedrosa@ccc.ufcg.edu.br>
2022-12-06 21:11:21 -0300

    primeiro commit

---
+++ src/main/java/chegamais/com/chagamais/ChagamaisApplication.java
@@ -0,0 +1,13 @@
+package chegamais.com.chagamais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChagamaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChagamaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chegamais/com/chagamais/ChagamaisApplicationTests.java
@@ -0,0 +1,13 @@
+package chegamais.com.chagamais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChagamaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

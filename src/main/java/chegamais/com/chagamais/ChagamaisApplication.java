adfc09dce461eee03bc2cc32749596c48fa922ef
Karen Aciole <karen.alves@ccc.ufcg.edu.br>
2022-12-17 13:04:08 -0300

    Swagger API Documentation

--- src/main/java/chegamais/com/chagamais/ChagamaisApplication.java
+++ src/main/java/chegamais/com/chagamais/ChagamaisApplication.java
@@ -2,7 +2,9 @@ package chegamais.com.chagamais;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class ChagamaisApplication {
 

78637836dd741fca35c6ef1e2f723e298bdb7d32
Karen Aciole <karen.alves@ccc.ufcg.edu.br>
2022-12-17 17:23:58 -0300

    feat: jwt in  app properties

---
+++ src/main/java/chegamais/com/chagamais/security/WebSecurityConfig.java
@@ -0,0 +1,16 @@
+package chegamais.com.chagamais.security;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true
+)
+public class WebSecurityConfig {
+
+}
+

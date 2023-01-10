34144f8e80627e3a937fbb2f945425e12d7a87ea
ekarani <ekarani.silvestre@ccc.ufcg.edu.br>
2023-01-10 16:48:22 -0300

    primeiro commit

---
+++ src/main/java/chegamais/com/chagamais/model/Horario.java
@@ -0,0 +1,16 @@
+package chegamais.com.chagamais.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class Horario {
+
+    public Horario {
+        
+    }
+}

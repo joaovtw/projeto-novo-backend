6510d281c540bd29d5bddd093f69b5dcdc51ad64
João Victor Teodulo Wanderley <44265869+joaovtw@users.noreply.github.com>
2023-02-02 23:47:14 -0300

    Update Event.java

--- src/main/java/chegamais/com/chagamais/model/Event.java
+++ src/main/java/chegamais/com/chagamais/model/Event.java
@@ -1,3 +1,4 @@
+
 package chegamais.com.chagamais.model;
 
 import javax.persistence.*;
@@ -17,9 +18,11 @@ public class Event {
     @NotNull @NotEmpty
     private final Usuario owner; // quem criou o evento
     //Usuário que deveria ter uma lista de eventos.
+
     @ManyToMany
     @JoinTable(name = "event_comfirmado", joinColumns = @JoinColumn(name = "event_id"), inverseJoinColumns = @JoinColumn(name = "comfirmado_id"))
     private List<Usuario> confirmados; // quem confirmou presença
+
     @NotNull @NotEmpty
     private LocalDateTime dataHora;
     @NotNull @NotEmpty
@@ -35,7 +38,73 @@ public class Event {
         this.duracao = duracao;
         this.isPublic = isPublic;
     }
-    public boolean addConfirmado(Usuario usuario) {
-        return confirmados.add(usuario);
+
+    public Long getId() {
+        return id;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Usuario getOwner(){
+        return this.owner;
+    }
+
+    public List<Usuario> getConfirmados() {
+        return confirmados;
+    }
+
+    public Usuario addConfirmado(Usuario user) {
+        if (!containsConfirmado(user)) {
+            this.confirmados.add(user);
+            return user;
+        }
+        return null;
+    }
+
+    public boolean containsConfirmado(Usuario user){
+        return this.confirmados.contains(user);
+    }
+
+    public Usuario removeComfirmado(Usuario user){
+        if (containsConfirmado(user)) {
+            this.confirmados.remove(user);
+            return user;
+        }
+        return null;
+    }
+
+    public LocalDateTime getDataHora() {
+        return this.dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora){
+        this.dataHora = dataHora;
+    }
+
+    public Duration getDuracao() {
+        return this.duracao;
+    }
+
+    public void setDuracao(Duration duracao) {
+        this.duracao = duracao;
+    }
+
+    public Boolean getPublic() {
+        return this.isPublic;
+    }
+
+    public void setPublic(Boolean aPublic) {
+        this.isPublic = aPublic;
+    }
+
 }
